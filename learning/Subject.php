29e56b6d4d440cf668c225e2e138f8caf0529796
Moses techcode <davidkapila30@gmail.com>
2025-10-05 15:39:35 +0200

    Add files via upload

---
+++ learning/Subject.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+  </head>
+  <body>
+<nav>
+  <?php
+  include_once("../mune.php")
+  ?>
+</nav>
+<div class="w-full h-[70vh] rounded-3xl mt-[70px] mb-[69px] bg-gray-300 fixed p-[24px] overflow-y-auto">
+  <h1 class="text-3xl font-bold text-center mb-6">Biology</h1>
+   <details class="mb-4 border rounded-2xl shadow-md bg-white">
+    <summary class="cursor-pointer px-4 py-3 text-lg font-semibold bg-gray-100 rounded-t-2xl">
+      introduction to Biology
+    </summary>
+    <div class="p-2">
+      <a href="note.php" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Biology</span>
+        <i class="lucide-dna w-5 h-5 text-green-600"></i>
+      </a>
+      <a href="#" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Physics</span>
+        <i class="lucide-atom w-5 h-5 text-blue-600"></i>
+      </a>
+      <a href="#" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Chemistry</span>
+        <i class="lucide-flask-conical w-5 h-5 text-purple-600"></i>
+      </a>
+    </div>
+  </details>
+   <details class="mb-4 border rounded-2xl shadow-md bg-white">
+    <summary class="cursor-pointer px-4 py-3 text-lg font-semibold bg-gray-100 rounded-t-2xl">
+      what is a ezame
+    </summary>
+    <div class="p-2">
+      <a href="Subject.php" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Biology</span>
+        <i class="lucide-dna w-5 h-5 text-green-600"></i>
+      </a>
+      <a href="#" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Physics</span>
+        <i class="lucide-atom w-5 h-5 text-blue-600"></i>
+      </a>
+      <a href="#" class="flex items-center justify-between w-full h-[50px] px-4 m-2 bg-gray-100 shadow rounded-xl font-medium hover:bg-gray-200 transition">
+        <span>Chemistry</span>
+        <i class="lucide-flask-conical w-5 h-5 text-purple-600"></i>
+      </a>
+    </div>
+  </details>
+<div class=""></div>
+  
+  </div>
+<nav>
+  <?php
+  include_once("../Bottom_mune.php")
+  ?>
+</nav>
+  </body>
+</html>
