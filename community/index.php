3f37faac708218f72f3d57495a5040abaedd92c1
Moses techcode <davidkapila30@gmail.com>
2025-10-05 15:56:26 +0200

    Add files via upload

--- community/index.php
+++ community/index.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>community</title>
+  </head>
+  <body>
+    <nav>
+      <?php 
+      include_once("../mune.php")
+      ?>
+    </nav>
+<div class=" w-full h-[70vh] rounded-3xl mt-[70px] mb-[69px] bg-gray-300 fixed p-[24px]">
 
+
+
+	</div>
+    <nav>
+      <?php 
+      include_once("Bottom_mune.php")
+      ?>
+    </nav>
+  </body>
+</html>
