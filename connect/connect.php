807730a369c3be0a1a2407495a9088aea8932717
Moses techcode <davidkapila30@gmail.com>
2025-10-05 15:53:05 +0200

    Add files via upload

--- connect/connect.php
+++ connect/connect.php
@@ -1,1 +1,10 @@
+<?php
+$host = "0.0.0.0";
+$user = "root";
+$pass = "root";
+$db_name = "learning_platform";
 
+if(!$con = mysql_connect($host,$user,$pass,$db_name)){
+  die("did not connecte successfully");
+}
+?>
