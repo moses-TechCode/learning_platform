e789745175e8ea4a9fd06f06c09779ab503d7d79
Moses techcode <davidkapila30@gmail.com>
2025-10-05 15:48:30 +0200

    Add files via upload

--- Formadder/submit.php
+++ Formadder/submit.php
@@ -1,1 +1,21 @@
+<?php
+include_once("../connect/connect.php")
+?>
 
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add_notes</title>
+  </head>
+  <body>
+    <form action="" method="post" accept-charset="utf-8">
+      <label for="subject">subject</label>
+      <input type="text" id="subject" name="" value="" />
+      <label for="notes">notes</label>
+      <textarea name="notes" id=notes rows="8" cols="40"></textarea>
+    </form>
+  </body>
+</html>
