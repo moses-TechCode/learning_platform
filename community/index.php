a7cd4531d85a18ed33eb7fb2a63d444413b9af43
Moses techcode <davidkapila30@gmail.com>
2025-10-20 22:15:44 +0200

    Add files via upload

--- community/index.php
+++ community/index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_email'])) {
+    header("Location:../ form_submition/login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,15 +12,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>community</title>
   </head>
-  <body>
+  <body class="bg-blue-900">
     <nav>
       <?php 
-      include_once("../mune.php")
+      include_once("mune.php")
       ?>
     </nav>
-<div class=" w-full h-[70vh] rounded-3xl mt-[70px] mb-[69px] bg-gray-300 fixed p-[24px]">
+<div class=" w-full h-[70vh] rounded-3xl mt-[70px] mb-[69px]  fixed p-[24px]">
 
-
+   <h1 class="font-bold text-6xl font-mono text-white text-start">
+   coming soon
+   </h1>
 
 	</div>
     <nav>
